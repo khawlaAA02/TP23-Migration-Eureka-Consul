b0086406a549164065287776de001358bbec6220
Khawla <khawlaaa02@gmail.com>
2026-01-01 17:24:48 +0100

    Initial commit - TP33 Eureka Consul Kubernetes

---
+++ src/test/java/com/example/demok8s/Tp33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demok8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
